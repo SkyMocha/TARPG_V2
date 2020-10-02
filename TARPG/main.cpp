8fc91eb8d74b0cd687b5297d515cda14b0983b1b
AntarcticRuler <34521078+AntarcticRuler@users.noreply.github.com>
2020-10-02 16:39:59 -0400

    Initial Commit

---
+++ TARPG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TARPG
+//
+//  Created by Nicolas Kychenthal on 10/2/20.
+//  Copyright © 2020 SkyMocha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
